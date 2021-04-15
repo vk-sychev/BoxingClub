fcdd2a93bdbc721586037d6de4c9fa27c439f942
Vasiliy Sychev <vasiliy.sychev@dataart.com>
2021-04-15 17:37:33 +0300

    Deleted migrations and comments

--- BoxingClub.DAL/Migrations/20210414113444_IntroducedNewRoles.cs
+++ BoxingClub.DAL/Migrations/20210414113444_IntroducedNewRoles.cs
@@ -14,46 +14,6 @@ namespace BoxingClub.DAL.Migrations
             migrationBuilder.DropTable(
                 name: "Coaches");
 
-/*            migrationBuilder.DeleteData(
-                table: "AspNetUserRoles",
-                keyColumns: new[] { "RoleId", "UserId" },
-                keyValues: new object[] { "5d73a819-a418-47b8-8c2d-c2e2ff6974db", "a0c933c1-2c8e-4919-a9b1-4187941b48dd" });
-
-            migrationBuilder.DeleteData(
-                table: "AspNetUserRoles",
-                keyColumns: new[] { "RoleId", "UserId" },
-                keyValues: new object[] { "5d73a819-a418-47b8-8c2d-c2e2ff6974db", "c00ded26-e898-462b-9374-e17433d618c9" });
-
-            migrationBuilder.DeleteData(
-                table: "AspNetUserRoles",
-                keyColumns: new[] { "RoleId", "UserId" },
-                keyValues: new object[] { "43ee9747-ff30-45e7-a3b6-3d1df4274879", "eb4bf1eb-37d2-4128-9e58-3debcaae344c" });
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "43ee9747-ff30-45e7-a3b6-3d1df4274879");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetRoles",
-                keyColumn: "Id",
-                keyValue: "5d73a819-a418-47b8-8c2d-c2e2ff6974db");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetUsers",
-                keyColumn: "Id",
-                keyValue: "a0c933c1-2c8e-4919-a9b1-4187941b48dd");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetUsers",
-                keyColumn: "Id",
-                keyValue: "c00ded26-e898-462b-9374-e17433d618c9");
-
-            migrationBuilder.DeleteData(
-                table: "AspNetUsers",
-                keyColumn: "Id",
-                keyValue: "eb4bf1eb-37d2-4128-9e58-3debcaae344c");*/
-
             migrationBuilder.AlterColumn<string>(
                 name: "CoachId",
                 table: "BoxingGroups",
@@ -98,8 +58,6 @@ namespace BoxingClub.DAL.Migrations
                 columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
                 values: new object[,]
                 {
-/*                    { "291c0120-8c27-47c5-83fe-9d7deb36f73c", "28c18050-2080-4daa-85d7-63181f16ca0e", "Admin", "ADMIN" },
-                    { "7bb8b4c7-de76-4b77-b5cf-ce4ef11d83a6", "4bce291f-b263-4af2-b909-b34f83fe140e", "Manager", "MANAGER" },*/
                     { "db460306-31c6-457a-989e-9e4317be99b9", "f22b0614-a9a2-4d39-bf69-4d569abcb44b", "User", "USER" },
                     { "8da509ca-2005-457d-8ca3-105792f04013", "1aa92723-3e34-4d8b-a67a-fdafa6f28345", "Coach", "COACH" }
                 });
@@ -108,10 +66,7 @@ namespace BoxingClub.DAL.Migrations
                 table: "AspNetUsers",
                 columns: new[] { "Id", "AccessFailedCount", "BornDate", "ConcurrencyStamp", "Description", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "Name", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "Patronymic", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "Surname", "TwoFactorEnabled", "UserName" },
                 values: new object[,]
-                {
-                    /*{ "fda7dfec-9828-41b2-bd9c-53dccbef2bb8", 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "7d6270c5-62cb-4598-90b8-712a18c47f88", null, "Manager1@gmail.com", false, false, null, null, "MANAGER1@GMAIL.COM", "MANAGER1", "AQAAAAEAACcQAAAAEBIOTZu5j+mlmvOT9mA5QEoaiHMMrUuhQpHVWur5GVxnGHdzvvN/xBzpjDtrldJcug==", null, null, false, "b934d20c-ebb8-4cde-b743-1e806647e5a3", null, false, "Manager1" },
-                    { "2d4254a5-7782-4b9c-a987-42a83d30669a", 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "0eef8bfd-0bfb-43b7-9888-6ddc8f37511d", null, "Manager2@gmail.com", false, false, null, null, "MANAGER2@GMAIL.COM", "MANAGER2", "AQAAAAEAACcQAAAAEErkYh50oJwNHDYsoo9qirg+3FOHtbvNZa1MS7GQ6YgF4MS9Hvd00lg3pd6w7d/0dA==", null, null, false, "95d7baf6-da67-4277-bb6c-bfa9f58d3825", null, false, "Manager2" },
-                    { "7dc730f1-78ec-41f5-a079-7d5e5d6b39ef", 0, new DateTime(2000, 10, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "9d877141-b47b-434b-b13d-08f4c7ca5447", null, "Admin@gmail.com", false, false, null, "Vasya", "ADMIN@GMAIL.COM", "ADMIN", "AQAAAAEAACcQAAAAEEcYBNPkGgCxhBa7RHEq4qdd3A1mHSZuXugT1e02Eu4rdI23i4oSD/v5WnWKMjXPnw==", "Konstantinovich", null, false, "08a1295e-5f60-41f2-a601-75d17a143c1f", "Sychev", false, "Admin" },*/
+                {                    
                     { "19759de3-ce1d-4cfd-8340-4e64eb245eb4", 0, new DateTime(1995, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "8ff9d236-d0d3-486b-8099-64260b7b2901", "CMS in boxing", "Coach1@gmail.com", false, false, null, "Pavel", "COACH1@GMAIL.COM", "COACH1", "AQAAAAEAACcQAAAAEO37bAmZ57QgDNQqz5hCm0fPpq5Y0e2RRUAajHxi7Dt5d+LJD6VfBqDZVxo4yJiHZg==", "Nikolayevich", null, false, "c4f444d9-57c0-4aa4-ac8c-b24b9deddf89", "Dorochin", false, "Coach1" },
                     { "060342c3-9dc3-4597-bae1-9f19c991ebe9", 0, new DateTime(1991, 7, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "ce4a3e53-a924-4c12-8083-229161279b44", "CMS in boxing", "Coach2@gmail.com", false, false, null, "Vlad", "COACH2@GMAIL.COM", "COACH2", "AQAAAAEAACcQAAAAEGEwVprG6C6orWnK1pdm4+llqFMyw1WKBOG/+YoNejaeNsGLgM4DbcsfTUbh7g6m/w==", "Nikolayevich", null, false, "66015801-47a3-4d61-aeb2-60dca6f2ec5a", "Dorochin", false, "Coach2" },
                     { "a50a06a5-df07-4728-b6a0-93173c2ce4cf", 0, new DateTime(1970, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "45ee15bb-8cf5-4043-a91d-86a798c10874", "MS in boxing", "Coach3@gmail.com", false, false, null, "Sergey", "COACH3@GMAIL.COM", "COACH3", "AQAAAAEAACcQAAAAEA/wCoxlJYH6Hpr05F6O8W4B/OgL5lkLqGURn7J4Vts53MypelCGu7/xNGh0J6N8vw==", null, null, false, "988ae349-ed99-4a46-9cc4-77706a9be935", "Goncharov", false, "Coach3" }
@@ -122,9 +77,6 @@ namespace BoxingClub.DAL.Migrations
                 columns: new[] { "RoleId", "UserId" },
                 values: new object[,]
                 {
-                    //{ "291c0120-8c27-47c5-83fe-9d7deb36f73c", "7dc730f1-78ec-41f5-a079-7d5e5d6b39ef" },
-                    //{ "7bb8b4c7-de76-4b77-b5cf-ce4ef11d83a6", "fda7dfec-9828-41b2-bd9c-53dccbef2bb8" },
-                    //{ "7bb8b4c7-de76-4b77-b5cf-ce4ef11d83a6", "2d4254a5-7782-4b9c-a987-42a83d30669a" },
                     { "8da509ca-2005-457d-8ca3-105792f04013", "19759de3-ce1d-4cfd-8340-4e64eb245eb4" },
                     { "8da509ca-2005-457d-8ca3-105792f04013", "060342c3-9dc3-4597-bae1-9f19c991ebe9" },
                     { "8da509ca-2005-457d-8ca3-105792f04013", "a50a06a5-df07-4728-b6a0-93173c2ce4cf" }
