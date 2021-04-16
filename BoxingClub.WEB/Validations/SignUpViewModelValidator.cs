a1bdb3e871e0ebac31c98e261e0d5754425eace3
vk-sychev <57107067+vk-sychev@users.noreply.github.com>
2021-04-16 14:37:08 +0300

    Done with toolbar

--- BoxingClub.WEB/Validations/SignUpViewModelValidator.cs
+++ BoxingClub.WEB/Validations/SignUpViewModelValidator.cs
@@ -1,6 +1,7 @@
 ﻿using BoxingClub.WEB.Models;
 using FluentValidation;
 using System;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,11 +10,15 @@ namespace BoxingClub.WEB.Validations
 {
     public class SignUpViewModelValidator : AbstractValidator<SignUpViewModel>
     {
-        public SignUpViewModelValidator()
+        private readonly IConfiguration _configuration;
+        public SignUpViewModelValidator(IConfiguration configuration)
         {
+            _configuration = configuration;
+
+            var passwowdLength = Convert.ToInt32(_configuration.GetSection("PasswordSettings").GetSection("RequiredLength").Value);
             RuleFor(x => x.UserName).NotNull().MinimumLength(5);
             RuleFor(x => x.Email).EmailAddress().NotNull().NotEmpty();
-            RuleFor(x => x.Password).NotNull().MinimumLength(3);
+            RuleFor(x => x.Password).NotNull().MinimumLength(passwowdLength);
             RuleFor(x => x.Password).Equal(x => x.ConfirmPassword).WithMessage("Password and confirmation password do not match.");
 
             var todaysDate = DateTime.Today;
