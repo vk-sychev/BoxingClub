c1007f627e23c3cf4e32c45469d9c7fda2b8a1e3
Vasiliy Sychev <vasiliy.sychev@dataart.com>
2021-05-19 19:12:28 +0300

    Doing Validation

---
+++ BoxingClub.Web/Validations/CreateEditTournamentViewModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using BoxingClub.Web.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoxingClub.Web.Validations
+{
+    public class CreateEditTournamentViewModelValidator : AbstractValidator<CreateEditTournamentViewModel>
+    {
+        public CreateEditTournamentViewModelValidator()
+        {
+            
+        }
+    }
+}
