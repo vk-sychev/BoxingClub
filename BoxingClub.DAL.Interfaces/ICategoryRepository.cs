796838d37db73151afb9fcae6797d9d9e6e8d317
Vasiliy Sychev <vasiliy.sychev@dataart.com>
2021-05-21 18:59:03 +0300

    Fixed all remarks except Identity

--- BoxingClub.DAL.Interfaces/ICategoryRepository.cs
+++ BoxingClub.DAL.Interfaces/ICategoryRepository.cs
@@ -1,8 +1,11 @@
 ﻿using BoxingClub.DAL.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BoxingClub.DAL.Interfaces
 {
     public interface ICategoryRepository:IRepository<Category>
     {
+        public Task<List<Category>> GetCategoriesByIds(List<Category> categoriesIds);
     }
 }
